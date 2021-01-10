1f40f73dcd3bebea15a77a58371275c79110d6bc
Basil K Y <techiebasil@gmail.com>
2021-01-10 20:36:14 +0530

    added test

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const fs = require('fs')
 const imageSplitter = require('./imageSplitter.js')
+;(async () => {
+  try {
+    const chunckSize = 500
+    const chuncks = await imageSplitter.imageToChunks(
+      'imgs/test.jpg',
+      chunckSize
+    )
+    console.log('Number of chunks', chuncks.length)
+    actions = []
+    console.log(chuncks)
+    let i = 0
+    chuncks.forEach(c => {
+      i++
+      actions.push(
+        fs.writeFile(`./imgs/slice_${i}.png`, c, function (err, result) {
+          if (err) console.log('error', err)
+        })
+      )
+    })
+    await Promise.all(actions)
+  } catch (e) {
+    console.log(e)
+  }
+})()
